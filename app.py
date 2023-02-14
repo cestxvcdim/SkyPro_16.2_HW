a01d23e0e6236671a6ecf0dfe40807cc3fb83560
vvvadim <v.vladimirov@it-park.tech>
2023-02-14 14:12:40 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JSON_AS_ASCII'] = False
+app.url_map.strict_slashes = False
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    email = db.Column(db.String(100))
+    role = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+
+    def to_dict(self):
+        return {getattr(self, col.name) for col in self.__table__.columns}
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    order = db.relationship("Order")
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor = db.relationship("User")
+
+    def to_dict(self):
+        return {getattr(self, col.name) for col in self.__table__.columns}
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text(100))
+    start_date = db.Column(db.String(100))
+    end_date = db.Column(db.String(100))
+    address = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+    customer_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    customer = db.relationship("User")
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    executor = db.relationship("User")
+
+    def to_dict(self):
+        return {getattr(self, col.name) for col in self.__table__.columns}
